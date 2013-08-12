80f3de4360d89be4a20eb4c92f96125f110ee48d
daniele <me@danielemargutti.com>
2013-08-12 09:14:27 +0200

    Add MIT License

--- NSSplitView+DMAdditions/NSSplitView+DMAdditions.h
+++ NSSplitView+DMAdditions/NSSplitView+DMAdditions.h
@@ -3,6 +3,13 @@
 //
 //  Created by Daniele Margutti on 12/21/12.
 //
+//	Licensed under MIT License
+//  Permission is given to use this source code file without charge in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
 
 #import <Cocoa/Cocoa.h>
 
